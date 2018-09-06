3bde881637b66610a7691a15a5c3aae31fe2b574
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-09-06 15:21:10 +0200

    Can get joke
    Gets joke in string format

--- Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
+++ Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
@@ -1,8 +1,17 @@
 package edu.ap.spring.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Scope("session")
@@ -31,7 +40,38 @@ public class JokeController {
    @RequestMapping("/joke_post")
    @ResponseBody
    public String joke_post(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
-	   return "";
+	   
+	   URL url;
+	   String joke = new String();
+	try {
+		url = new URL("http://api.icndb.com/jokes/random?firstName=" + firstname + "&lastName=" + lastname);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Content-Type", "application/json");
+		
+		BufferedReader in = new BufferedReader(
+				  new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer content = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+				    content.append(inputLine);
+				}
+				in.close();
+				
+		joke =content.toString();
+				
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   
+	   StringBuilder builder = new StringBuilder();
+	   
+	   builder.append("<html><body><p>");
+	   builder.append(joke);
+	   builder.append("</p></body></html>");
+	   
+	   return builder.toString();
    }
    
    @RequestMapping("/")
