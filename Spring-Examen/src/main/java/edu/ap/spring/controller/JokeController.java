ef6a7b207a500e935fc871284da5e6fb7a39e47a
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-09-06 14:27:33 +0200

    Submitpage init
    Get Joke page has been made

--- Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
+++ Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
@@ -12,12 +12,25 @@ public class JokeController {
    }
        
    @RequestMapping("/joke")
+   @ResponseBody
    public String joke() {
-	   return "";
+	   StringBuilder builder = new StringBuilder();
+	   
+	   builder.append("<html><body><h1>Get a joke</h1>" + ""
+	   		+ "<form action='joke_post' method ='POST'>\r\n" + 
+	   		"  First name:<br>\r\n" + 
+	   		"  <input type=\"text\" name=\"firstname\"><br>\r\n" + 
+	   		"  Last name:<br>\r\n" + 
+	   		"  <input type=\"text\" name=\"lastname\">\r\n" +
+	   		"  <input type=\"submit\" value=\"Submit\">" +
+	   		"  </form></body></html>");
+	   
+	   return builder.toString();
    }
 		   
    @RequestMapping("/joke_post")
-   public String joke_post() {
+   @ResponseBody
+   public String joke_post(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
 	   return "";
    }
    
